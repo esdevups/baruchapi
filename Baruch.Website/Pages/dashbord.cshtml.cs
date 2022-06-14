228e82f9691939a163af2207de5ea346371f4fd2
esAmir <amirhosseines03@gmail.com>
2022-06-14 16:43:42 +0430

    added some pages with mistakes

---
+++ Baruch.Website/Pages/dashbord.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Baruch.Website.Pages
+{
+    public class dashbordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Baruch.Website/wwwroot/Site/js/slider.js
+++ Baruch.Website/wwwroot/Site/js/slider.js
@@ -1,18 +1,38 @@
-let sliderImages = [];
+﻿const footerList = document.getElementById('footer-list');
 const slider = document.getElementById('index-slider');
- async function fetchImages () {
+async function fetchImages() {
     const response = await fetch("https://localhost:7196/api/ui");
+
+    console.log(response);
     const data = await response.json();
     console.log(data);
-    sliderImages = data;
-    console.log(sliderImages)
 
-    for (image of sliderImages) {
-        slider.innerHTML += `<div class="slide">
-            <img src="/Site/img/laura-adai-4tcF4Fe9HiI-unsplash_2.jpg"
+
+    if (slider) {
+
+    slider.innerHTML += `
+            <div class="slide">
+                <img src="https://api.baruch.ir/images/${data.image1}"
                  alt="" />
-        </div>`
+            </div>
+            <div class="slide">
+             <img src="https://api.baruch.ir/images/${data.image2}"
+                 alt="" />
+              </div>
+                <div class="slide">
+             <img src="https://api.baruch.ir/images/${data.image3}"
+                 alt="" />
+                 </div>
+`;
     }
+
+    footerList.innerHTML = ` <li class="py-1">
+                        آدرس: ${data.adress}
+                    </li>
+                    <li class="py-1">
+                        تلفن: 0915555555
+                    </li>`
+    
 };
 
 fetchImages();
@@ -24,7 +44,7 @@ const slides = document.querySelectorAll(".slide");
 
 // loop through slides and set each slides translateX property to index * 100% 
 slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${indx * 100}%)`;
+    slide.style.transform = `translateX(${indx * 100}%)`;
 });
 
 // select next slide button
@@ -39,35 +59,35 @@ let maxSlide = slides.length - 1;
 
 nextSlide.addEventListener("click", nextSlideHandler);
 
-function nextSlideHandler () {
+function nextSlideHandler() {
     // check if current slide is the last and reset current slide
     if (curSlide === maxSlide) {
-      curSlide = 0;
+        curSlide = 0;
     } else {
-      curSlide++;
+        curSlide++;
     }
-  
-  //   move slide by -100%
+
+    //   move slide by -100%
     slides.forEach((slide, indx) => {
-      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
-  }
+}
 // select prev slide button
 const prevSlide = document.querySelector(".btn-prev");
 
 // add event listener and navigation functionality
 prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--
-  }
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--
+    }
 
-  //   move slide by 100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
 });
 
 
@@ -78,15 +98,15 @@ let automove = setInterval(nextSlideHandler, 4000)
 
 //   stopping the interval if mouse is on slider 
 
-for(slide of slides) {
+for (slide of slides) {
 
     slide.addEventListener('mouseenter', () => {
-        
+
         clearInterval(automove)
     })
 
     slide.addEventListener('mouseleave', () => {
-        
+
         automove = setInterval(nextSlideHandler, 4000)
     })
 }
